8303c22e2180c2bc980e87c59cf1d86cde9b8fe4
Wunkolo <Wunkolo@gmail.com>
2016-09-17 12:36:24 -0700

    Moved Threading string include to relevant source file

--- Osiris/Libraries/Ausar/Threading/Threading.cpp
+++ Osiris/Libraries/Ausar/Threading/Threading.cpp
@@ -1,22 +1,23 @@
 #include "Threading.hpp"
+#include <string>
 
 namespace Ausar
 {
-	namespace Threading
-	{
-		uint64_t ThreadTable::GetThreadIDByName(const char * Name) const
-		{
-			for (size_t i = 0; i < 64; i++)
-			{
-				if (Entries[i].Active)
-				{
-					if (std::strcmp(Entries[i].ThreadName, Name) == 0)
-					{
-						return Entries[i].ThreadID;
-					}
-				}
-			}
-			return -1;
-		}
-	}
+    namespace Threading
+    {
+        uint64_t ThreadTable::GetThreadIDByName(const char * Name) const
+        {
+            for( size_t i = 0; i < 64; i++ )
+            {
+                if( Entries[i].Active )
+                {
+                    if( std::strcmp(Entries[i].ThreadName, Name) == 0 )
+                    {
+                        return Entries[i].ThreadID;
+                    }
+                }
+            }
+            return -1;
+        }
+    }
 }
--- Osiris/Libraries/Ausar/Threading/Threading.hpp
+++ Osiris/Libraries/Ausar/Threading/Threading.hpp
@@ -1,27 +1,27 @@
 #pragma once
-#include <string>
+#include <stdint.h>
 
 namespace Ausar
 {
-	namespace Threading
-	{
-		#pragma pack(push,1)
-		struct ThreadTable
-		{
-			struct ThreadEntry
-			{
-				uint8_t Active;
-				// Null terminated ascii
-				char ThreadName[0x40];
-				uint8_t Padding[7];
-				// Priority? Thread state?
-				uint64_t Unknown48;
-				uint64_t ThreadID;
-			} Entries[64];
+    namespace Threading
+    {
+#pragma pack(push,1)
+        struct ThreadTable
+        {
+            struct ThreadEntry
+            {
+                uint8_t Active;
+                // Null terminated ascii
+                char ThreadName[0x40];
+                uint8_t Padding[7];
+                // Priority? Thread state?
+                uint64_t Unknown48;
+                uint64_t ThreadID;
+            } Entries[64];
 
-			// returns -1 on error
-			uint64_t GetThreadIDByName(const char* Name) const;
-		};
-		#pragma pack(pop)
-	}
+            // returns -1 on error
+            uint64_t GetThreadIDByName(const char* Name) const;
+        };
+#pragma pack(pop)
+    }
 }
