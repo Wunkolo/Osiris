6948f94b5f289b1296fd726ff42ea3d941d02040
Wunkolo <Wunkolo@gmail.com>
2016-10-22 23:49:42 -0700

    Tucked away all code to adjust to new update
    Leaving in some of the old-shitty aob-scan test stuff commented out for completeness

--- Osiris/Osiris.cpp
+++ Osiris/Osiris.cpp
@@ -64,117 +64,133 @@ Osiris::Osiris()
     //}
     //);
 
-    InitTagNames();
+    //InitTagNames();
 
-    LOG << "Finding pattern" << std::endl;
+    //LOG << "Finding pattern" << std::endl;
 
-    Run = 1;
-    std::thread CycleThread(
-        []()
-    {
-        while( Run != 0 )
-        {
-            ModelMutex.lock();
-            ModelIDMutex.lock();
-            for( const auto& ModelPtr : ModelArray )
-            {
-                // Assign each Model pointer another random model
-                ModelPtr.Write<uint32_t>(ModelIDArray[rand() % ModelIDArray.size()]);
-            }
-            ModelIDMutex.unlock();
-            ModelMutex.unlock();
-            std::this_thread::sleep_for(std::chrono::milliseconds(125));
-        }
-    }
-    );
-    CycleThread.detach();
+    //Run = 1;
+    //std::thread CycleThread(
+    //    []()
+    //{
+    //    while( true )
+    //    {
+    //        ModelMutex.lock();
+    //        ModelIDMutex.lock();
+    //        for( const auto& ModelPtr : ModelArray )
+    //        {
+    //            // Assign each Model pointer another random model
+    //            ModelPtr.Write<uint32_t>(ModelIDArray[rand() % ModelIDArray.size()]);
+    //        }
+    //        ModelIDMutex.unlock();
+    //        ModelMutex.unlock();
+    //        std::this_thread::sleep_for(std::chrono::milliseconds(125));
+    //    }
+    //}
+    //);
+    //CycleThread.detach();
 
-    Util::Process::IterateReadableMemory(
-        [](Util::Pointer Base, size_t Size) -> bool
-    {
-        __try
-        {
-            [&]() {//LOG << "Region: " << Base << " | " << Size << std::endl;
-            //7F0000???????? global_id ????????00000000tagclassruntime_id
-                static const std::regex const Expression(
-                    R"(\x7F\x00\x00[\x00-\xFF]{12}[\x00]{4}\x20tam)",
-                    std::regex::optimize | std::regex::nosubs
-                );
+    //Util::Process::IterateReadableMemory(
+    //    [](Util::Pointer Base, size_t Size) -> bool
+    //{
+    //    [&]()
+    //    {//LOG << "Region: " << Base << " | " << Size << std::endl;
+    //    //7F0000???????? global_id ????????00000000tagclassruntime_id
+    //        static const std::regex const Expression(
+    //            R"(\x7F\x00\x00[\x00-\xFF]{12}[\x00]{4}jorp)",
+    //            std::regex::optimize | std::regex::nosubs
+    //        );
 
-                std::cregex_iterator Start = std::cregex_iterator(
-                    Base.Point<const char>(),
-                    Base(Size).Point<const char>(),
-                    Expression
-                );
+    //        std::cregex_iterator Start = std::cregex_iterator(
+    //            Base.Point<const char>(),
+    //            Base(Size).Point<const char>(),
+    //            Expression
+    //        );
 
-                std::cregex_iterator End = std::cregex_iterator();
+    //        std::cregex_iterator End = std::cregex_iterator();
 
-                for( std::cregex_iterator i = Start; i != End; i++ )
-                {
-                    const std::cmatch Match = *i;
-                    uint32_t GlobalID = Base(Match.position())(7).Read<uint32_t>();
-                    uint32_t RuntimeID = Base(Match.position())(23).Read<uint32_t>();
-                    const char * Name = GetTagNameFromGlobalID(GlobalID);
-                    if( Name == nullptr )
-                    {
-                        Name = "Not found";
-                    }
-                    LOG << "Offset: " << Base(Match.position()) << std::endl;
-                    LOG << "\tName: " << Name << std::endl;
-                    LOG << "\tGlobalID: " << GlobalID << std::endl;
-                    LOG << "\tRuntimeID: " << RuntimeID << std::endl;
+    //        for( std::cregex_iterator i = Start; i != End; ++i )
+    //        {
+    //            const std::cmatch Match = *i;
 
-                    if(
-                        (strstr(Name, R"(objects\equipment)") != nullptr)
-                        || (strstr(Name, R"(levels\assets)") != nullptr)
-                        || (strstr(Name, R"(levels\multi)") != nullptr)
-                        || (strstr(Name, R"(levels\forge)") != nullptr)
-                        || (strstr(Name, R"(objects\vehicles)") != nullptr)
-                        || (strstr(Name, R"(objects\weapons)") != nullptr)
-                        || (strstr(Name, R"(objects\characters)") != nullptr)
-                        || (strstr(Name, R"(objects\levels)") != nullptr)
-                        || (strstr(Name, R"(objects\temp)") != nullptr)
-                        )
-                    {
-                        LOG << "==========================Added to list" << std::endl;
-                        ModelIDMutex.lock();
-                        ModelIDArray.push_back(Base(Match.position())(23).Read<uint32_t>());
-                        // Remove duplicates
-                        std::set<uint32_t> Unique(ModelIDArray.begin(), ModelIDArray.end());
-                        ModelIDArray.assign(Unique.begin(), Unique.end());
-                        ModelIDMutex.unlock();
+    //            uint32_t GlobalID;
+    //            uint32_t RuntimeID;
 
-                        ModelMutex.lock();
-                        ModelArray.push_back(Base(Match.position())(23));
-                        ModelMutex.unlock();
-                    }
-                }
-            }();
-        }
-        __except( [&](LPEXCEPTION_POINTERS p) -> int
-        {
-            LOG << "yikes at " << Base << "!!!" << std::endl;
-            return EXCEPTION_CONTINUE_SEARCH;
-        }(GetExceptionInformation()) )
-        {
-            return false;
-        }
-        return true;
-    }
-    );
+    //            [&]()
+    //            {
+    //                __try
+    //                {
+    //                    GlobalID = Base(Match.position())(7).Read<uint32_t>();
+    //                    RuntimeID = Base(Match.position())(23).Read<uint32_t>();
+    //                }
+    //                __except( GetExceptionCode() == EXCEPTION_ACCESS_VIOLATION ?
+    //                    EXCEPTION_EXECUTE_HANDLER : EXCEPTION_CONTINUE_SEARCH )
+    //                {
+    //                    [&]()
+    //                    {
+    //                        LOG << "Yikes at" << Base << "!!!" << std::endl;
+    //                    }();
 
-    LOG << "Done" << std::endl;
+    //                    GlobalID = 0;
+    //                    RuntimeID = 0;
+    //                }
+    //            }();
+
+    //            const char * Name = GetTagNameFromGlobalID(GlobalID);
+    //            if( Name == nullptr )
+    //            {
+    //                Name = "Not found";
+    //            }
+    //            LOG << "Offset: " << Base(Match.position()) << std::endl;
+    //            LOG << "\tName: " << Name << std::endl;
+    //            LOG << "\tGlobalID: " << GlobalID << std::endl;
+    //            LOG << "\tRuntimeID: " << RuntimeID << std::endl;
+
+    //            if(
+    //                (strstr(Name, R"(objects)") != nullptr)
+    //                //(strstr(Name, R"(.sound)") != nullptr)
+    //                //&& (strstr(Name, R"(sound\001_vo)") == nullptr)//if its vo, skip it
+    //                //(strstr(Name, R"(objects\equipment)") != nullptr)
+    //                //|| (strstr(Name, R"(levels\assets)") != nullptr)
+    //                //|| (strstr(Name, R"(levels\multi)") != nullptr)
+    //                //|| (strstr(Name, R"(levels\forge)") != nullptr)
+    //                //|| (strstr(Name, R"(objects\vehicles)") != nullptr)
+    //                //|| (strstr(Name, R"(objects\weapons)") != nullptr)
+    //                //|| (strstr(Name, R"(objects\characters)") != nullptr)
+    //                //|| (strstr(Name, R"(objects\levels)") != nullptr)
+    //                //|| (strstr(Name, R"(objects\temp)") != nullptr)
+    //                )
+    //            {
+    //                LOG << "==========================Added to list" << std::endl;
+    //                ModelIDMutex.lock();
+    //                ModelIDArray.push_back(RuntimeID);
+    //                // Remove duplicates
+    //                std::set<uint32_t> Unique(ModelIDArray.begin(), ModelIDArray.end());
+    //                ModelIDArray.assign(Unique.begin(), Unique.end());
+    //                ModelIDMutex.unlock();
+
+    //                ModelMutex.lock();
+    //                ModelArray.push_back(Base(Match.position())(23));
+    //                ModelMutex.unlock();
+    //            }
+    //        }
+    //    }();
+
+    //    return true;
+    //}
+    //);
+
+    //LOG << "Done" << std::endl;
 
     //Spartan = 0;
 
     // Push Commands
-    PushModule<LogModule>("logging");
-    PushModule<GlobalInfo>("globals");
+    //PushModule<LogModule>("logging");
+    //PushModule<GlobalInfo>("globals");
     PushModule<PackageDump>("dump");
 
     TODO("Config file to enable dumping of files");
     // Uncomment this to enable dumping
-    //RunModule("dump");
+    RunModule("dump");
 }
 
 Osiris::~Osiris()
